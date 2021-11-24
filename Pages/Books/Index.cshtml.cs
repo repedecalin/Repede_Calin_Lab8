89b26776c0b865a63e38e42ab573806a5fa9b0ff
CALIN GABRIEL REPEDE <repede.calin174086@student.ubbcluj.ro>
2021-11-24 17:29:45 +0200

    Add project files.

---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Repede_Calin_Lab8.Data;
+using Repede_Calin_Lab8.Models;
+using Repede_Calin_Lab8.Pages;
+namespace Repede_Calin_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Repede_Calin_Lab8.Data.Repede_Calin_Lab8Context _context;
+
+        public IndexModel(Repede_Calin_Lab8.Data.Repede_Calin_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.Include(b=>b.Publisher).ToListAsync();
+        }
+    }
+}
---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Repede_Calin_Lab8.Data;
+using Repede_Calin_Lab8.Models;
+
+namespace Repede_Calin_Lab8.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Repede_Calin_Lab8.Data.Repede_Calin_Lab8Context _context;
+
+        public IndexModel(Repede_Calin_Lab8.Data.Repede_Calin_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Category.ToListAsync();
+        }
+    }
+}
